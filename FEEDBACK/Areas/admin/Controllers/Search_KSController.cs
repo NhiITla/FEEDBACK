e326596320a2e33f955972b64a71d767c4c9fe6e
Huyen Nhi <nhiitctu@gmail.com>
2020-08-17 16:18:54 +0700

    NHI- đã xong chức năng tìm kiếm theo mã loại Khách sạn

---
+++ FEEDBACK/Areas/admin/Controllers/Search_KSController.cs
@@ -0,0 +1,25 @@
+﻿using FEEDBACK.Models;
+using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Linq;
+
+namespace FEEDBACK.Areas.admin.Controllers
+{
+
+    public class Search_KSController : Controller
+    {
+        // GET: admin/Search_KS
+        public CSDL_FEEDBACK db = new CSDL_FEEDBACK();
+        [HttpGet]
+        public ActionResult Search(string searching)
+        {
+
+            var danhmuc_ks = db.Danhmuc_KS.Where(n => n.maloai_KS == searching).ToList();
+            return View(danhmuc_ks);
+        }
+    }
+}
--- FEEDBACK/Areas/admin/adminAreaRegistration.cs
+++ FEEDBACK/Areas/admin/adminAreaRegistration.cs
@@ -20,14 +20,28 @@ namespace FEEDBACK.Areas.admin
             );
 
             context.MapRoute(
+                "Admin Index",
+                "admin/{controller}/{action}/{id}",
+                new { controller = "Admin", action = "Index", id = UrlParameter.Optional }
+            );
+
+            context.MapRoute(
                 "Edit",
                 "admin/{controller}/{action}/{id}",
                 new { controller="Danhmuc_KS" ,action = "Index", id = UrlParameter.Optional }
             );
+
+
+
             context.MapRoute(
                 "Tim kiem KS",
                 "admin/{controller}/{action}/{id}",
                 new { controller = "Timkiem_KS", action = "Index", id = UrlParameter.Optional }
+            );
+            context.MapRoute(
+                "Tim kiem ma loai",
+                "admin/{controller}/{action}/{id}",
+                new { controller = "Search_KS", action = "Search", id = UrlParameter.Optional }
             );
 
 
